3b8dd6aadb9003039a4998c3f531f66b6122fe49
Vanessa Fotso <vfotso@mitre.org>
2023-04-13 10:50:20 -0400

    getiting organizations

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class DashboardController < ApplicationController
-  before_action :require_cp_client, :set_tasks
+  before_action :require_cp_client, :set_tasks, :get_cbo_organizations
 
   # GET /dashboard
   def main
@@ -7,20 +7,27 @@ class DashboardController < ApplicationController
   end
 
   private
+
   # Getting all resources associated with the given patient
 
   def set_tasks
     success, result = fetch_tasks
     if success
       @active_cp_tasks = result["cp_tasks"]&.dig("active") || []
-      @completed_cp_tasks= result["cp_tasks"]&.dig("completed") || []
-      @canceled_cp_tasks = result["cp_tasks"]&.dig("canceled") || []
+      @completed_cp_tasks = result["cp_tasks"]&.dig("completed") || []
+      @cancelled_cp_tasks = result["cp_tasks"]&.dig("cancelled") || []
       @active_ehr_tasks = result["ehr_tasks"]&.dig("active") || []
       @completed_ehr_tasks = result["ehr_tasks"]&.dig("completed") || []
-      @canceled_ehr_tasks = result["ehr_tasks"]&.dig("canceled") || []
+      @cancelled_ehr_tasks = result["ehr_tasks"]&.dig("cancelled") || []
     else
       flash[:warning] = result
     end
   end
 
+  def get_cbo_organizations
+    @cbo_organizations = Rails.cache.fetch("cbo", expires_in: 1.day) do
+      @fhir_cp_client.read_feed(FHIR::Organization)&.resource&.entry&.map(&:resource).select { |o| o.name != "ABC Coordination Platform" }
+    end
+    @cbo_organizations ||= []
+  end
 end
